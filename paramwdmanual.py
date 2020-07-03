1c73ea24271a26c98962f4b60d21c7147dbb0ab1
pere-prlpz <prlpzb@gmail.com>
2020-07-03 14:08:57 +0200

    paramwd versió manual pels BRL
    paramwdman.py: versió del paramwd.py per posar manualment el paràmetre wd copiant i enganxant de la llista en castellà. Simplifica el workflow respecte a copiar entre el codi wiki d'una pàgina i l'altra.

---
+++ paramwdmanual.py
@@ -0,0 +1,54 @@
+# Ajuda per copiar manualment el paràmetre wikidata a les llistes de monuments
+# Preparat per copiar i enganxar els BRL a les llistes valencianes a partir d'eswiki.
+# L'únic paràmetre és el nom de la llista:
+# python paramwdman.py "llista de monuments de l'Alt Maestrat"
+# Les cometes són opcionals.
+
+import pywikibot as pwb
+import mwparserfromhell
+import sys
+import re
+
+def actuallistaman(pllista):
+    origen=pllista.title()
+    text=pllista.get()
+    text0=text
+    code = mwparserfromhell.parse(text)
+    t=code.filter_templates();
+    #print(t)
+    for template in t:
+        posat = False
+        if template.name.matches(("filera BIC Val")):
+            if template.has("wikidata"):
+                wd=template.get("wikidata").value.strip()
+                wd=re.sub("<!-- no ([Ww][Dd] )?((auto|amb bot) )?-->", "", wd).strip()
+                #print(wd)
+            else:
+                wd=""
+            if wd=="" and template.has("prot"):
+                if re.match("BRL", template.get("prot").value.strip()):
+                    print (template.get("nomcoor").value.strip())
+                    entrada = input("Wikidata: ")
+                    if re.match("[Qq].+", entrada):
+                        template.add("wikidata",entrada.upper().strip())
+    text=code
+    if text != text0:
+        print("Desant",pllista)
+        pllista.put(text,u"Robot actualitza manualment el paràmetre wikidata")
+    else:
+        print("Cap canvi")
+    return()
+
+
+# el programa comença aquí
+arguments = sys.argv[1:]
+if len(arguments)>0:
+    nomllista=" ".join(arguments)
+else:
+    print("Manca el nom de la llista de monuments. Agafem opció per defecte")
+    nomllista="Llista de monuments del Baix Maestrat"
+site=pwb.Site('ca')
+pag = pwb.Page(site, nomllista)
+#pag = pwb.Page(site, "Usuari:PereBot/taller")
+print (pag)
+actuallistaman(pag)
